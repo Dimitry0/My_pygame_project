fe1f49add7a82dedfd491cd44f4452f325a13fe3
Dimitry <petrovdima1256@gmail.com>
2021-12-30 02:07:30 +0300

    release v1.1

--- game.py
+++ game.py
@@ -1,33 +1,8 @@
 import pygame
-# import pygame_menu
+
 # from pygame.locals import *
 
 pygame.init()
-
-# заготовка менюшки
-
-# surface = pygame.display.set_mode((600, 400))
-
-
-# def set_difficulty(value, difficulty):
-#     Do the job here !
-#    pass
-
-
-# def start_the_game():
-#   Do the job here !
-#   pass
-
-
-# menu = pygame_menu.Menu('Welcome', 400, 300,
-#                       theme=pygame_menu.themes.THEME_BLUE)
-
-# menu.add.text_input('Name :', default='John Doe')
-# menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-# menu.add.button('Play', )
-# menu.add.button('Quit', pygame_menu.events.EXIT)
-
-# menu.mainloop(surface)
 
 
 clock = pygame.time.Clock()
